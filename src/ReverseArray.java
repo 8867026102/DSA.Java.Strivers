7cf7b14823c2a89c505da45708a7941efc27e1a2
bhoomikabk <bkbhoomika4@gmail.com>
2026-09-05 16:23:00 +0530

    init commit

---
+++ src/ReverseArray.java
@@ -0,0 +1,20 @@
+public class ReverseArray {
+    static void func(int[] arr, int i , int n) {
+        if(i>=n/2) {
+            return;
+        }
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+        func(arr, i+1, n );
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5,6};
+        int n = arr.length;
+        func(arr,0,n);
+        for(int i=0;i<n;i++) {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
