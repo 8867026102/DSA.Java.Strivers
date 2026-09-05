df858f7a7ab3c222ebf822892bbc04307aad6fd9
bhoomikabk <bkbhoomika4@gmail.com>
2026-09-05 16:54:21 +0530

    init commit

---
+++ src/day1/CheckPalindrome.java
@@ -0,0 +1,28 @@
+package day1;
+import java.util.*;
+public class CheckPalindrome {
+    static boolean f(String str,int i){
+        if(i>=str.length()/2){
+            return true;
+        }
+        if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+
+            return false;
+        }
+        return f(str,i+1);
+    }
+
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str = sc.nextLine();
+        if(f(str,0)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+        sc.close();
+
+
+    }
+}
